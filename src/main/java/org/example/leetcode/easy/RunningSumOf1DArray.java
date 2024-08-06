fe68a28112ef1f721812a5698f3d42342060bdd5
Casper <asilbek2001.05@gmail.com>
2024-08-06 13:26:46 +0500

    1480. Running Sum of 1d Array

--- src/main/java/org/example/leetcode/easy/RunningSumOf1DArray.java
+++ src/main/java/org/example/leetcode/easy/RunningSumOf1DArray.java
@@ -8,8 +8,8 @@ public class RunningSumOf1DArray {
         int sum=0;
         int len = nums.length;
         int[] res=new int[len];
-        for (int i = 0; i < len; i++) {
-            sum+=nums[i];
+        for (int num : nums) {
+            sum += num;
         }
         res[len-1]=sum;
         for (int i = 0; i < len-1; i++) {
