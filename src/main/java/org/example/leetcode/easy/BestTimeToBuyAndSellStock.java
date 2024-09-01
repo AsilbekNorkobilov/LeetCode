13ad540c03bc99aa1e19af9a7ea469430f1dc458
Casper <asilbek2001.05@gmail.com>
2024-09-01 17:38:35 +0500

    121. Best Time to Buy and Sell Stock

--- src/main/java/org/example/leetcode/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/example/leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -5,11 +5,11 @@ public class BestTimeToBuyAndSellStock {
         int[] prices={2,1,4};
         int profit=0;
         int min=Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i]<min){
-                min=prices[i];
-            }else if (prices[i]-min>profit){
-                profit=prices[i]-min;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (price - min > profit) {
+                profit = price - min;
             }
         }
         System.out.println(profit);
