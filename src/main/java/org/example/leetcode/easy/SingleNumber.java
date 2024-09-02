b6dba9364543853be3e4bb9fadd3eef1404a34a2
Casper <asilbek2001.05@gmail.com>
2024-09-02 17:48:26 +0500

    136. Single Number

---
+++ src/main/java/org/example/leetcode/easy/SingleNumber.java
@@ -0,0 +1,23 @@
+package org.example.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums={4,2,1,1,2};
+        int res=0;
+        Map<Integer,Integer> counter=new HashMap<>();
+        for (int num : nums) {
+            counter.put(num,counter.getOrDefault(num,0)+1);
+        }
+        for (Integer i : counter.keySet()) {
+            if (counter.get(i)==1){
+                res=i;
+            }
+        }
+        System.out.println(res);
+
+    }
+}
